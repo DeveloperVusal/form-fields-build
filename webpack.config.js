030b53e62bc0fff9ce132c48fc5bddc0967a0006
DESKTOP-QT2NDDE\Vusal <developer.vusal96@gmail.com>
2021-11-28 22:15:21 +0300

    Added Babel and editing webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/index.js',
+    mode: 'production', // Собираем сразу для Продакшн версии
+    entry: './src/index.js', // Входный файл для сборки
     output: {
-        path: path.resolve(__dirname, 'public/dist'),
-        filename: 'form-fields-build.min.js',
-        publicPath: 'public/'
+        path: path.resolve(__dirname, 'public/dist'), // Папка для выгрузки бандла
+        filename: 'form-fields-build-0.0.1.min.js', // Название собранного бандла
+        publicPath: 'public/',
+        clean: true, // При каждой сборке очищаем
     },
     module: {
         rules: [
@@ -15,6 +17,16 @@ module.exports = {
                     'style-loader',
                     'css-loader'
                 ]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             }
         ]
     }
