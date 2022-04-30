6aeff39cd51f5290ed5ed4f9b1f83923332464a4
DeveloperVusal <developer.vusal96@gmail.com>
2022-04-30 16:57:57 +0300

    Генерация html формы
    Была реализована простая генерация html формы (для тестов)

--- src/components/init.js
+++ src/components/init.js
@@ -3,5 +3,16 @@
  * @param {*} options 
  */
 export const init = (options) => {
-    console.log(options)
+    let html = ''
+
+    for (let item of options.fields) {
+        html += `
+            <div style="display:block;">
+                <label for="${item.name}">${item.label}</label>
+                <input id="${item.name}" type="${item.type}" name="${item.name}" value="${(item.value) ? item.value : ''}" />
+            </div>
+        `
+    }
+
+    document.querySelector(options.el).innerHTML = html
 }
