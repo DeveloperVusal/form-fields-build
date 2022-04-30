ea81f42b0e2762dea95005eb23d3376062881ef5
DeveloperVusal <developer.vusal96@gmail.com>
2022-04-30 17:20:19 +0300

    Рефакторин формы
    Переработка вызова объекта сборщика формы

--- src/components/init.js
+++ src/components/init.js
@@ -3,16 +3,22 @@
  * @param {*} options 
  */
 export const init = (options) => {
-    let html = ''
+    let fields = ''
 
     for (let item of options.fields) {
-        html += `
-            <div style="display:block;">
+        fields += `
+            <div style="display: block;">
                 <label for="${item.name}">${item.label}</label>
                 <input id="${item.name}" type="${item.type}" name="${item.name}" value="${(item.value) ? item.value : ''}" />
             </div>
         `
     }
+
+    const html = `
+        <div style="margin: 4rem auto; width: 25%">
+            ${fields}
+        </div>
+    `
 
     document.querySelector(options.el).innerHTML = html
 }
